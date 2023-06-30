2c8482b3324408af0438582ebee0d94ebee7ff8d
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-06-30 17:22:55 +0200

    start

--- main.c
+++ main.c
@@ -6,31 +6,48 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 15:13:19 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/06/29 18:03:55 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/06/30 17:22:13 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
-//#include <sys/types.h> 
-//#include <sys/wait.h> 
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int	main(int ac, char **av)
 {
 	pid_t	father;
 	pid_t	wait();
-	
-	father = fork();
-	if (father > 0)
+	char	*t = getenv("VTE_VERSION");
+	printf("%s\n", t);	
+	while (1)
 	{
-		wait();
-		printf("I'M FATHER\n");
-
-	}
-	if (father == 0)
-	{
-		sleep(10);
-		execve("/bin/ls", av, NULL);
+		static char	*s = (char *)NULL; 
+		if (s)
+		{
+			free(s);
+			s = (char *)NULL;
+		}
+		s = readline("");
+		father = fork();
+		if (s)
+		{
+			if (father > 0)
+			{
+				wait();
+				//printf("%s\n", s);
+			}
+			if (father == 0)
+			{
+				if (!strcmp(s, "ls"))
+					execve("/bin/ls", av, NULL);
+				if (!strcmp(s, "pwd"))
+					execve("/bin/pwd", av, NULL);
+			}
+		}
 	}
 	return (0);
 }
+
