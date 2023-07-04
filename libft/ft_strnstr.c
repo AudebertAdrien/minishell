be00bc8f6f6942c146ea200805620272ba4a72f4
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-07-04 15:22:40 +0200

    prompt done

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,28 +6,11 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 14:54:31 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/02/26 19:07:28 by motoko           ###   ########.fr       */
+/*   Updated: 2023/07/04 15:09:17 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*
-	DESCRIPTION
-	The strnstr() function locates the first occurrence of the 
-	null-terminated string little in the string big, 
-	where not more than len characters are searched.
-	Characters that appear after a ‘\0’ character are not
-     	searched.  Since the strnstr() function 
-	is a FreeBSD specific API, 
-	it should only be used when portability is not a concern.
-
-	RETURN VALUES
-     	If little is an empty string, big is returned; if little occurs 
-	nowhere in big, NULL is returned; otherwise a
-     	pointer to the first character of the first 
-	occurrence of little is returned.
-*/
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
