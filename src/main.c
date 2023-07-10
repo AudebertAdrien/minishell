3916b3fa0778844738609bf76075f1b47af44448
adrien audebert <audebertadrien.pro@gmail.com>
2023-07-10 15:33:59 +0200

    save

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mcreus <mcreus@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 15:13:19 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/07/10 15:29:58 by motoko           ###   ########.fr       */
+/*   Updated: 2023/07/10 15:32:50 by motoko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int	main(int ac, char **av, char **env)
 			if (process > 0)
 				wait();
 			if (process == 0)
-		parse_readline(str);
+		parse_readline(str, env);
 		}
 	}
 	return (0);
