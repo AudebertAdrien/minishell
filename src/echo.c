120674627a0725aee677df5ffe6f116df05dc6c0
adrien audebert <audebertadrien.pro@gmail.com>
2023-07-27 14:01:01 +0200

    echo in progress

--- src/echo.c
+++ src/echo.c
@@ -6,21 +6,44 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 15:02:13 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/07/12 16:42:04 by motoko           ###   ########.fr       */
+/*   Updated: 2023/07/27 13:57:45 by motoko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	echo(char **tab)
+int	is_double_quote(char *str)
 {
 	int	i;
+	int	len;
+
+	i = 0;
+	len = ft_strlen(str);
+	if (str[0] == '"' && str[len - 1] == '"')
+		return (1);
+	return (0);
+}
+
+int	echo(char **tab, char **env)
+{
+	int	i;
+	char	*str;
 
 	i = 1;
 	while (tab[i])
 	{
-		ft_putstr_fd(tab[i], 1);
-		ft_putchar_fd('\n', 1);
+		if (is_double_quote(tab[i]))
+		{
+//			printf("USER %s\n", ft_get_env(env, "USER"));
+//			printf("1 %s\n", ft_substr(tab[i], 1, ft_strlen(tab[i]) - 2));
+//			printf("2 %s\n", getenv(ft_substr(tab[i], 1, ft_strlen(tab[i] - 2)) ));
+			str =  ft_substr(tab[i], 1, ft_strlen(tab[i]) - 2);
+			printf("2 %s\n", getenv(str));
+
+		}
+		//ft_putstr_fd(tab[i], 1);
+		if (ft_strncmp(tab[1], "-n", 3))
+			ft_putchar_fd('\n', 1);
 		i++;
 	}
 	return (0);
--- src/find_cmd.c
+++ src/find_cmd.c
@@ -6,7 +6,7 @@
 /*   By: mcreus <mcreus@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 17:44:53 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/07/20 13:08:39 by motoko           ###   ########.fr       */
+/*   Updated: 2023/07/27 13:26:03 by motoko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	find_cmd(char **tab, char **envcpy)
 
 	cmd = tab[0];
 	if (!strcmp(cmd, "echo"))
-		echo(tab);
+		echo(tab, envcpy);
 	else if (!strcmp(cmd, "cd"))
 		ft_cd(tab, envcpy);
 	else if (!strcmp(cmd, "pwd"))
